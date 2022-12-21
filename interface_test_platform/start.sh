f95746217fa0b7bd4996549837695ccdde776afb
MarcoJones <279958437@qq.com>
2022-12-21 16:18:36 +0800

    Update start.sh

--- interface_test_platform/start.sh
+++ interface_test_platform/start.sh
@@ -1,17 +1,1 @@
- #!/bin/bash
-base_dir=/home/jones/interface_test_platform
-res='ls -A $base_dir'
-jenkins_dir=$WORKSPACE/interface_test_platform
-# 判断部署目录是否为空
-if [-z $res];then
-# 如果为空直接拷贝部署文件到部署目录
-cp -r $jenkins_dir/* $base_dir/
-echo pwd
-else
-# 不为空先清理部署目录再拷贝部署
-rm -rf $base_dir/*
-cp -r $jenkins_dir/* $base_dir/
-echo pwd
-fi
-cd $base_dir/
-nohup python3 manage.py runserver 0.0.0.0:8000 > /dev/null   2>&1 &
+sudo nohup python3 manage.py runserver 0.0.0.0:8000 >djo.out 2>&1 &
