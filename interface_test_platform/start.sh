f17970458b77bf2bd95c9fc38e095f7adbc47e31
MarcoJones <279958437@qq.com>
2022-12-21 12:03:52 +0800

    Update start.sh
    添加部署脚本

--- interface_test_platform/start.sh
+++ interface_test_platform/start.sh
@@ -1,3 +1,19 @@
  #!/bin/bash
-
+base_dir=/home/jones/interface_test_platform
+res='ls -A $base_dir'
+jenkins_dir=$WORKSPACE/interface_test_platform
+# 判断部署目录是否为空
+if [-z $res];then
+# 如果为空直接拷贝部署文件到部署目录
+cp -r $jenkins_dir/* $base_dir/
+echo pwd
+cd $base_dir/
 python3 manage.py runserver
+else
+# 不为空先清理部署目录再拷贝部署
+rm -rf $base_dir/*
+cp -r $jenkins_dir/* $base_dir/
+echo pwd
+cd $base_dir/
+fi
+nohup python manage.py runserver 0.0.0.0:8000 > /dev/null   2>&1 &
